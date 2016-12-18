a28eca835d35b91c9181a72542c150942a537c1c
jonahandamy <jonahandamy@comcast.net>
2016-12-18 15:02:01 -0600

    Remove form
    Php form doesn’t work. Change to coming soon.

--- js/contact_me.js
+++ js/contact_me.js
@@ -11,7 +11,6 @@ $(function() {
             var name = $("input#name").val();
             var email = $("input#email").val();
             var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
@@ -24,7 +23,6 @@ $(function() {
                     name: name,
                     phone: phone,
                     email: email,
-                    message: message
                 },
                 cache: false,
                 success: function() {
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -15,9 +15,9 @@ $phone = $_POST['phone'];
 // Create the email and send the message
 $to = 'jonahandamy@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Song request from $name";
-$email_body = "Someone sent you a song request for your wedding!\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone";
+$email_body = "Someone sent you a song request for your wedding!\n\n"."Here are the details:\n\nName: $name\n\nSong title: $email_address\n\nArtist: $phone";
 $headers = "From: noreply@jonahandamy.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+$headers .= "Reply-To: jonahandamy@gmail.com";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
